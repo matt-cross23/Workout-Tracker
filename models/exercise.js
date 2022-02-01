df37367e51b0b6f38394a284f9a1da230b03ea69
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-01 14:09:35 -0500

    add model

--- models/exercise.js
+++ models/exercise.js
@@ -1,3 +1,26 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a workout"
+  },
+  value: {
+    type: Number,
+    required: "Enter the weight amount"
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
+
+module.exports = Exercise;
