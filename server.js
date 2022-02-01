68afc9adc057d2578897308ef972d4d1016ddfda
Matthew Cross <matthewcross1017@gmail.com>
2022-01-31 23:54:12 -0500

    Connect Front and Backend

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.urlencoded({extended: true}));
-app.use(express.static("public");
+app.use(express.static("public"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",{
         useNewURLParser: true,
         useFindAndModify: false
         });
-app.use(require("./routes/api.js")
+app.use(require("./routes/api.js"));
                 
 app.listen(PORT, () =>{
   console.log(`Your App is listening on port ${PORT}!`);
