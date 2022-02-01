8087faa5053125f8d27af1a82ae170ca37597202
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-01 14:13:26 -0500

    add Create CRUD operation

--- routes/api.js
+++ routes/api.js
@@ -1,2 +1,9 @@
 const router = require("express").Router();
+router.get("/api/exercise", (req, res) => {
+  Exercise.find({})
+    .sort({ date: -1 })
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    });
+});
 module.exports = router;
