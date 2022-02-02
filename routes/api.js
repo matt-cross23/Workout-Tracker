7a0fcfbfab77eeaad022ceec83db5c81935c8f35
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-02 14:50:02 -0500

    Add Routes

--- routes/api.js
+++ routes/api.js
@@ -1,29 +1,9 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Exercise = require("../models/exercise.js");
+const router = express.Router();
 
-router.get("/api/workouts", (req, res) => {
-  Exercise.find({})
-  .limit(7)
-    .sort({ date: -1 })
-    .then((dbExercise) => {
-      res.json(dbExercise);
-    });
-  .catch((err => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/exercise", ({body}, res){
-Exercise.create(body)
-.then(dbExercise => {
-res.json(dbExercise}
-         })
-.catch(err => {
-  res.status(400).json(err);
-  });
-});
-
+// Create a new Workout
 router.post("/api/exercise", ({body}, res) => {
 Exercise.create(body)
 .then(dbTransaction =>{
@@ -33,6 +13,30 @@ res.json(dbExercise}
       res.status(400).json(err);
     });
 });
+// Read all of the exercises in past week
+router.get("/api/workouts/weekly", (req, res) => {
+  Exercise.find({})
+  .limit(7)
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    });
+  .catch((err => {
+      res.status(400).json(err);
+    });
+});
+
+// Read all exercises
+router.get("/api/workouts", (req, res){
+Exercise.create({})
+.then(dbExercise => {
+res.json(dbExercise}
+         })
+.catch(err => {
+  res.status(400).json(err);
+  });
+});
+
+
 
 
 module.exports = router;
