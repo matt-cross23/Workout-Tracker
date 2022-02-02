5ce6a2b5f34a561c49a246300c1680ad8bab7d65
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-02 13:56:57 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,25 @@ const Exercise = require("../models/exercise.js");
 
 router.get("/api/workouts", (req, res) => {
   Exercise.find({})
+  limit(5)
     .sort({ date: -1 })
     .then((dbExercise) => {
       res.json(dbExercise);
     });
-  .catch((err))
+  .catch((err => {
+      res.status(400).json(err);
+    });
 });
+
+router.post("/api/exercise", ({body}, res) => {
+Exercise.create(body)
+.then(dbTransaction =>{
+res.json(dbExercise}  
+})
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
 module.exports = router;
