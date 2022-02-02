ad9bc1807bb66a3349b722f24c17a193a22cb21d
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-02 13:42:28 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,13 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
+const Exercise = require("../models/exercise.js");
+
 router.get("/api/exercise", (req, res) => {
   Exercise.find({})
     .sort({ date: -1 })
     .then((dbExercise) => {
       res.json(dbExercise);
     });
+  .catch((err))
 });
 module.exports = router;
