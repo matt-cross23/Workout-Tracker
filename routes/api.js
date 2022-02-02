01211d07fde2aaa976d97ff0065916fadf9d957c
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-02 13:43:14 -0500

    change endpoint to match frontend

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const Exercise = require("../models/exercise.js");
 
-router.get("/api/exercise", (req, res) => {
+router.get("/api/workouts", (req, res) => {
   Exercise.find({})
     .sort({ date: -1 })
     .then((dbExercise) => {
