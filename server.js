418ec7dc24f3e6b9d77d6dcdae10ca2247d9422a
Matthew Cross <88978036+matt-cross23@users.noreply.github.com>
2022-02-03 15:08:15 -0500

    Add morgan

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
 const PORT = process.env.PORT || 3000;
+
 const app = express();
+app.use(logger("dev"));
+
 app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout-Tracker",{
